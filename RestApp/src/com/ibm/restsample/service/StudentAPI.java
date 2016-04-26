eccd84d2010ba41b17d27f8e0eb7a1917a951234
Varada Thakur <varada.thakur@in.ibm.com>
2016-04-26 15:15:00 +0530

    no conflict

--- RestApp/src/com/ibm/restsample/service/StudentAPI.java
+++ RestApp/src/com/ibm/restsample/service/StudentAPI.java
@@ -6,5 +6,5 @@ public interface StudentAPI {
 	Student getName();
 	Student setName(Student newStudent);
 	Student getAge();
-	Student setStudent(Student newstudent);
+	Student setStudent(Student newstudent, int age);
 }
