c25a195f3680983de3184e7d81bfc65fbe964ce5
var-th <tvarada@yahoo.com>
2016-04-26 16:11:37 +0530

    server change

--- RestApp/src/com/ibm/restsample/service/StudentAPI.java
+++ RestApp/src/com/ibm/restsample/service/StudentAPI.java
@@ -6,5 +6,5 @@ public interface StudentAPI {
 	Student getName();
 	Student setName(Student newStudent);
 	Student getAge();
-	Student setStudent(Student newstudent, int age);
+	Student setStudent(int age);
 }
