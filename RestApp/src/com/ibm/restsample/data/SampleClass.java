a67eed440ece1dff48c13a27104e501f5228ebdd
Varada Thakur <varada.thakur@in.ibm.com>
2016-04-28 20:04:59 +0530

    Added new file

---
+++ RestApp/src/com/ibm/restsample/data/SampleClass.java
@@ -0,0 +1,13 @@
+package com.ibm.restsample.data;
+
+public class SampleClass {
+	private int count;
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+}
