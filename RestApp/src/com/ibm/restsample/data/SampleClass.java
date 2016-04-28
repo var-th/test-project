555588c71a1ff9749a523beb9a48f8d7ce4ee0b0
var-th <tvarada@yahoo.com>
2016-04-28 20:29:49 +0530

    Added a attribute

--- RestApp/src/com/ibm/restsample/data/SampleClass.java
+++ RestApp/src/com/ibm/restsample/data/SampleClass.java
@@ -2,6 +2,7 @@ package com.ibm.restsample.data;
 
 public class SampleClass {
 	private int cnt;
+	private String name
 
 	public int getCount() {
 		return cnt;
@@ -9,5 +10,13 @@ public class SampleClass {
 
 	public void setCount(int count) {
 		this.cnt = count;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(final String name) {
+		this.name = name;
 	}
 }
